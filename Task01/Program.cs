8a673f07cecf932353654a24fdfc6f2ec07ebd92
Andrew_Kochubeeev <andrew-k-44@mail.ru>
2023-02-23 14:58:14 +0300

    add task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b*b) Console.WriteLine("Является квадратом");
+else Console.WriteLine("Не является квадратом");
