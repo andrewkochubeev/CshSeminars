176ed577088ce09e43ee1e201b7fa4803e3b9ead
andrew_kochubeev <andrew-k-44@mail.ru>
2023-03-01 21:32:48 +0300

    Add Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая
+// принимает на вход трёхзначное
+// число и на выходе показывает
+// вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99 && num < 1000) Console.WriteLine(num % 100 / 10);
+else Console.WriteLine("Число нетрёхзначное");
