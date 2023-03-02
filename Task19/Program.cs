c406359c528ab52263eda39ea66b4c8ea6f8234e
andrew_kochubeev <andrew-k-44@mail.ru>
2023-03-02 16:16:02 +0300

    Add Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000 || number < -9999 && number > -100000)
+{
+    Console.WriteLine(IsPalindrome(number) ? "Да" : "Нет");
+}
+else Console.WriteLine("Число не пятизначное");
+
+int DigitOfNumber(int num, int numDigit)
+{
+
+    for (int i = 0; i < 5 - numDigit; i++)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+
+}
+
+bool IsPalindrome(int num)
+{
+    return DigitOfNumber(num, 1) == DigitOfNumber(num, 5) && DigitOfNumber(num, 2) == DigitOfNumber(num, 4);
+}
