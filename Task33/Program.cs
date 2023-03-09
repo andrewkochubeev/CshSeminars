b1c40218511439c85e7c7fb1359ce653eee5dbcd
andrew_kochubeev <andrew-k-44@mail.ru>
2023-03-09 14:18:21 +0300

    Add Task 33

---
+++ Task33/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool SearchDigit(int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Введите искомое число:");
+int digit = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(10, -9, 9);
+PrintArray(array);
+Console.WriteLine(SearchDigit(digit, array) ? "Да" : "Нет");
