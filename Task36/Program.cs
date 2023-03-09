ee8780b917b235fa7cad86df6b1a203da0044949
andrew_kochubeev <andrew-k-44@mail.ru>
2023-03-09 16:10:49 +0300

    Add Task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+int SumNumOddPos(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите размер массива:");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(sizeArr, -50, 50);
+PrintArray(array);
+int sumNumOddPos = SumNumOddPos(array);
+Console.WriteLine($"Сумма чисел на нечетных позициях = {sumNumOddPos}");
