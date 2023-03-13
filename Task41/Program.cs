d22e0c8fb108f8323e5ed69e7c58b4492451b3ef
Andrew_Kochubeeev <andrew-k-44@mail.ru>
2023-03-13 20:24:03 +0300

    Add Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с
+// клавиатуры M чисел. Посчитайте, сколько
+// чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] EnterArray(int size)
+{
+    int[] arr = new int[size];
+    Console.WriteLine("Вводите числа");
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+int CountPosArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число М:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = EnterArray(m);
+Console.WriteLine($"Количество положительных чисел = {CountPosArray(array)}");
